9bed177041a3729acc505b2bc012599ceb93a16c
Steffen Stein <therealstein@tutanota.com>
2022-06-16 17:01:23 +1000

    added

--- index.js
+++ index.js
@@ -5,23 +5,33 @@ async function run() {
   // The YML workflow will need to set myToken with the GitHub Secret Token
   // myToken: ${{ secrets.GITHUB_TOKEN }}
   // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-  const myToken = core.getInput('myToken');
+  let myToken = core.getInput('myToken');
+  if (myToken) {
+    console.log(myToken);
+  } else {
+    require('dotenv').config();
+    console.log(process.env.GITHUB_ACTION);
+    myToken = {
+      type: 'token',
+      token: process.env.GITHUB_ACTION,
+      tokenType: 'oauth',
+    };
+  }
 
   const octokit = github.getOctokit(myToken);
-
-  // You can also pass in additional options as a second parameter to getOctokit
-  // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
-
+  console.log(myToken);
   const { data: pullRequest } = await octokit.rest.pulls.get({
-    owner: 'octokit',
-    repo: 'rest.js',
-    pull_number: 123,
+    owner: 'therealstein',
+    repo: 'release-multirepo',
+    pull_number: 13,
     mediaType: {
       format: 'diff',
     },
   });
-
   console.log(pullRequest);
+
+  // You can also pass in additional options as a second parameter to getOctokit
+  // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
 }
 
 run();
